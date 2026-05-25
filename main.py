35020439205760886eff28b4709c70bc1c22064c
ariane <ariane@LAPTOP-V1676G45>
2026-05-25 14:03:11 +0800

    Updated

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from typing import List
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from model_training import (
     train_model,
@@ -9,10 +11,37 @@ from model_training import (
 
 app = FastAPI()
 
+# ALLOW FRONTEND TO CONNECT
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-# =========================
+@app.post("/evaluate-raw")
+async def evaluate_raw(data: dict):
+
+    gesture = data.get("gesture", [])
+
+    if len(gesture) < 1:
+        return {
+            "error": "No gesture points received"
+        }
+
+    start_time = gesture[0]["time"]
+    end_time   = gesture[-1]["time"]
+
+    duration_ms = round(end_time - start_time, 2)
+
+    return {
+        "duration_ms": duration_ms,
+        "points": len(gesture)
+    }
+
+
 # REQUEST MODELS
-# =========================
 
 class TrainRequest(BaseModel):
     X_data: List[List[float]]
@@ -23,9 +52,7 @@ class EvaluateRequest(BaseModel):
     features: List[float]
 
 
-# =========================
 # ROOT
-# =========================
 
 @app.get("/")
 def home():
@@ -34,9 +61,7 @@ def home():
     }
 
 
-# =========================
 # BUILD / TRAIN MODEL
-# =========================
 
 @app.post("/build-model")
 def build_model(data: TrainRequest):
@@ -52,9 +77,7 @@ def build_model(data: TrainRequest):
     }
 
 
-# =========================
 # EVALUATE GESTURE
-# =========================
 
 @app.post("/evaluate")
 def evaluate(data: EvaluateRequest):
